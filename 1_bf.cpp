7087dd150faea251a6bb787b285b558782d8277a
Evan Le Valley <holmcross@users.noreply.github.com>
2019-06-29 22:11:18 -0700

    Add files via upload

---
+++ 1_bf.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    //brute force
+    std::vector<int> twoSum(vector<int>& nums, int target) {
+        int sum = 0;
+        for(int i = 0 ; i<nums.size()-1 ; i++){
+            for(int j = i+1 ; j<nums.size() ; j++){
+                if(nums[i] + nums[j] == target){
+                    std::vector<int> vect{i,j};
+                    return vect;
+                }
+            }
+        }
+        std::vector<int> blah{0,0};
+        return blah;
+    }
+};
